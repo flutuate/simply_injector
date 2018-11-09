e401a7add625507cbf9000721c704a88c4eca859
Luciano Rodrigues <luciano.rodrigues@ailos.coop.br>
2018-11-09 18:44:27 -0200

    Implemented new register methods; Added new exceptions; Add new tests with Container register.

---
+++ lib/src/ArgumentException.dart
@@ -0,0 +1,13 @@
+class ArgumentException
+implements Exception
+{
+  final String _message;
+  final String _paramName;
+
+  ArgumentException(this._message, this._paramName);
+
+  @override
+  String toString() {
+    return '$runtimeType: $_message\nParameter name: $_paramName';
+  }
+}
